113428d5e53e5790042f8135c3d015d489cb9493
Tyler Eppley <teppley300@gmail.com>
2023-06-06 12:40:25 -0400

    centered text buttons and image

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
+    <footer className="w-100 mt-auto text-dark p-4 flex flex-col items-center">
       <div className="container text-center mb-5">
         {location.pathname !== '/' && (
           <button
