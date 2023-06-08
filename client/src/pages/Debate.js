677724379471fb4954d4d72bd52f9849b1ec1a47
Devin Reilly <werthird2016@gmail.com>
2023-06-08 16:00:44 -0400

    websocket to debate page

--- client/src/pages/Debate.js
+++ client/src/pages/Debate.js
@@ -1,7 +1,8 @@
 import { useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useParams, useLocation } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 import ProfileList from '../components/ProfileList';
 
@@ -12,6 +13,9 @@ import Footer from '../components/Footer';
 
 
 const Debate = () => {
+    const lobby = useLocation().state;
+    console.log(lobby);
+
     const socket = io('http://localhost:3001'); // Replace with your server URL
 
     const { lobbyId } = useParams();
@@ -24,11 +28,11 @@ const Debate = () => {
     const joinLobby = () => {
         socket.emit('join_lobby', `${lobbyId}`)
         console.log(`user joined ${lobbyId}`)
-    }
+    };
     joinLobby();
 
     const sendMessage = () => {
-        socket.emit('client_message', message);
+        socket.emit('client_message', currentMessage);
         setMessage('');
     };
 
@@ -36,9 +40,9 @@ const Debate = () => {
         const receiveMessage = (data) => {
             setMessageList((list) => [...list, data]);
           };
-          socket.on('receive_message', receiveMessage);
+          socket.on('server_message', receiveMessage);
           return () => {
-            socket.off('receive_message', receiveMessage);
+            socket.off('server_message', receiveMessage);
           };
     }, [socket]);
 
@@ -63,27 +67,33 @@ const Debate = () => {
                 </div>
 
                 <div className="content border-2 border-black h-96">
-                    <h2 className='pl-4'>Hello</h2>
+                    <h2 className='pl-4'>Hello, you are in lobby ID: {lobbyId}</h2>
+                    <h2 className='pl-4'>Lobby topic: {lobby.topic}</h2>
+                    <h2 className='pl-4'>Lobby host: {lobby.host}</h2>
 
                     <div className="messages w-full px-5 flex flex-col justify-between">
-                        <div class="flex flex-col mt-5">
-                            <div class="flex justify-end mb-4">
-                        {messageList.map((messageContent) => {
-                            return (
-                                <div>
-                                    <h3>{messageContent.message}</h3>
-                                    <span>{messageContent.time} </span>
-                                    <span>Debator One {messageContent.username}</span>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div>
+                        <div className="flex flex-col mt-5">
+                            <div className="flex flex-col justify-end mb-4 border-2 border-black">
+                                {messageList.map((messageContent) => {
+                                    return (
+                                        <p key={uuidv4()}>{messageContent}</p>
+                                    )
+                                })}
+                            </div>
+                        <div>
                     </div>
                     </div>
                         <div className='py-5 flex mt-52'>
-                        <input className='w-full bg-gray-300 py-5 px-3 rounded-xl' type='text' placeholder='Message...' onChange={(e) => setCurrentMessage(e.target.value)} />
-                        <button className='bg-gradient-to-br from-zinc-600 text- to-cyan-300 text-black px-4 py-2 mr-5 border-none rounded-md ml-12 hover:animate-pulse' onClick={sendMessage}>Send Message</button>
+                        <input 
+                            className='w-full bg-gray-300 py-5 px-3 rounded-xl' 
+                            type='text' 
+                            placeholder='Message...' 
+                            onChange={(e) => setCurrentMessage(e.target.value)} 
+                        />
+                        <button 
+                            className='bg-gradient-to-br from-zinc-600 text- to-cyan-300 text-black px-4 py-2 mr-5 border-none rounded-md ml-12 hover:animate-pulse' 
+                            onClick={sendMessage}
+                        >Send Message</button>
                         </div>
                     {/* {username === admin || username === debatorOne || username === debatorTwo ? (    
                     ) : (
