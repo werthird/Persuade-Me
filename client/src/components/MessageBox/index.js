a2168528f6613e274ed1d288981595a8298e224d
tylervanderstaay <117237435+tylervanderstaay@users.noreply.github.com>
2023-06-09 20:22:22 -0400

    fixing files to merge

--- client/src/components/MessageBox/index.js
+++ client/src/components/MessageBox/index.js
@@ -4,37 +4,37 @@ import { SEND_MESSAGE } from '../../utils/mutations';
 
 const MessageBox = ({ socket, lobby, author, chatHistory }) => {
 
-  const lobbyId = lobby._id;
-  console.log(typeof lobbyId);
-  let staff = [...lobby.teamA, ...lobby.teamB, ...lobby.admin];
-  const role = lobby.teamA.includes(author)
-    ? 'teamA'
-    : lobby.teamB.includes(author)
-    ? 'teamB'
-    : 'admin';
-  const [message, setMessage] = useState('');
-  const [messageList, setMessageList] = useState([]);
-  const [currentMessage, setCurrentMessage] = useState('');
-  const [addMessage, { error }] = useMutation(SEND_MESSAGE);
-  const [showEmojis, setShowEmojis] = useState(false);
+    const lobbyId = lobby._id;
+    console.log(typeof lobbyId);
+    let staff = [...lobby.teamA, ...lobby.teamB, ...lobby.admin];
+    const role = lobby.teamA.includes(author)
+        ? 'teamA'
+        : lobby.teamB.includes(author)
+            ? 'teamB'
+            : 'admin';
+    const [message, setMessage] = useState('');
+    const [messageList, setMessageList] = useState([]);
+    const [currentMessage, setCurrentMessage] = useState('');
+    const [addMessage, { error }] = useMutation(SEND_MESSAGE);
+    const [showEmojis, setShowEmojis] = useState(false);
+    const lastElement = document.getElementById('lastElement')
+    const emojis = ['😀', '😄', '😊', '👍', '👋', '❤️', '🖕', '🤣', '✅', '☠️', '🤩'];
 
-  const emojis = ['😀', '😄', '😊', '👍', '👋', '❤️', '🖕', '🤣', '✅', '☠️', '🤩'];
-
-  const sendMessage = async () => {
-    const data = {
-      lobby: lobbyId,
-      author: author,
-      role: role,
-      contents: currentMessage,
+    const sendMessage = async () => {
+        const data = {
+            lobby: lobbyId,
+            author: author,
+            role: role,
+            contents: currentMessage,
         };
         try {
             const newMessage = await addMessage({ variables: data });
             console.log(newMessage)
             await socket.emit('client_message', newMessage.data.sendMessage);
             setMessageList((list) => [...list, newMessage.data.sendMessage]);
-            try{
-            lastElement.scrollIntoView({behavior:'smooth'})
-            }catch{
+            try {
+                lastElement.scrollIntoView({ behavior: 'smooth' })
+            } catch {
                 console.log('nah bro')
             }
         } catch (err) {
@@ -46,18 +46,18 @@ const MessageBox = ({ socket, lobby, author, chatHistory }) => {
 
 
 
-  const handleEmojiClick = (emoji) => {
-    setCurrentMessage((prevMessage) => prevMessage + emoji);
-  };
+    const handleEmojiClick = (emoji) => {
+        setCurrentMessage((prevMessage) => prevMessage + emoji);
+    };
 
     useEffect(() => {
         const receiveMessage = (data) => {
             setMessageList((list) => [...list, data]);
-            try{
-                lastElement.scrollIntoView({behavior:'smooth'})
-                }catch{
-                    console.log('nah bro')
-                }
+            try {
+                lastElement.scrollIntoView({ behavior: 'smooth' })
+            } catch {
+                console.log('nah bro')
+            }
         };
         socket.on('server_message', receiveMessage);
         return () => {
@@ -66,7 +66,7 @@ const MessageBox = ({ socket, lobby, author, chatHistory }) => {
     }, [socket]);
 
     return (
-        <div className="messages w-full px-5 flex flex-col justify-between ">
+        <div className="messages w-full px-5 flex flex-col justify-between">
             <div className="flex flex-col mt-5 max-h-vw">
                 <div className=" flex flex-col mb-4 overflow-y-scroll">
                     {chatHistory && chatHistory.messages.map((message) => {
@@ -89,58 +89,46 @@ const MessageBox = ({ socket, lobby, author, chatHistory }) => {
                     })}
                     <div id='lastElement'> </div>
                 </div>
-              );
-            })}
-          {messageList &&
-            messageList.map((message) => {
-              return (
-                <div className={message.role} key={message._id}>
-                  <p className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">{message.contents}</p>
-                  <h4 className='text-center ml-40 font-semibold'>{message.author}<h6 className='text-center text-xs text-gray-500'>{message.timestamp}</h6></h4>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-      <div className="py-5 flex">
-        <div className="relative w-full">
-          <input
-            className="w-full bg-gray-300 py-5 px-3 rounded-xl pr-12"
-            type="text"
-            placeholder="Message..."
-            value={currentMessage}
-            onChange={(e) => setCurrentMessage(e.target.value)}
-          />
-          <button
-            className=" absolute right-0 top-0 bottom-0 bg-gray-300 text-gray-700 px-4 py-2 border-none rounded-md ml-3 "
-            onClick={() => setShowEmojis(!showEmojis)}
-          >
-            😀
-          </button>
-          
-        </div>
-        {showEmojis && (
-            <div className="emoji-button cursor-pointer">
-              {emojis.map((emoji) => (
-                <span
-                  key={emoji}
-                  className="emoji"
-                  onClick={() => handleEmojiClick(emoji)}
-                >
-                  {emoji}
-                </span>
-              ))}
             </div>
-          )}
-        <button
-          className="bg-gradient-to-br from-zinc-600 text- to-cyan-300 text-black px-4 py-2 mr-5 border-none rounded-md ml-3 hover:animate-pulse"
-          onClick={sendMessage}
-        >
-          Send
-        </button>
-      </div>
-    </div>
-  );
+            <div className="py-5 flex">
+                <div className="relative w-full">
+                    <input
+                        className="w-full bg-gray-300 py-5 px-3 rounded-xl pr-12"
+                        type="text"
+                        placeholder="Message..."
+                        value={currentMessage}
+                        onChange={(e) => setCurrentMessage(e.target.value)}
+                    />
+                    <button
+                        className=" absolute right-0 top-0 bottom-0 bg-gray-300 text-gray-700 px-4 py-2 border-none rounded-md ml-3 "
+                        onClick={() => setShowEmojis(!showEmojis)}
+                    >
+                        😀
+                    </button>
+
+                </div>
+                {showEmojis && (
+                    <div className="emoji-button cursor-pointer">
+                        {emojis.map((emoji) => (
+                            <span
+                                key={emoji}
+                                className="emoji"
+                                onClick={() => handleEmojiClick(emoji)}
+                            >
+                                {emoji}
+                            </span>
+                        ))}
+                    </div>
+                )}
+                <button
+                    className="bg-gradient-to-br from-zinc-600 text- to-cyan-300 text-black px-4 py-2 mr-5 border-none rounded-md ml-3 hover:animate-pulse"
+                    onClick={sendMessage}
+                >
+                    Send
+                </button>
+            </div>
+        </div >
+    );
 };
 
 export default MessageBox;
